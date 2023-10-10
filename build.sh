a1a3cc97c7a036669bcc828813fb0f7479b55a16
CobbCoding <rircobb@gmail.com>
2023-10-10 16:14:15 -0400

    updated build to include a -t option, which builds all the tests

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 set -xe
 gcc tim.c time.c -o time -Wall -Wextra
 gcc tasm.c  tasmlexer.c tasmparser.c tim.c -o tasm -Wall -Wextra
+
+while getopts "t" flag; do
+ case $flag in
+   t)
+       for file in tests/*.tasm
+       do
+           ./tasm $file
+       done
+   ;;
+   \?)
+   ;;
+ esac
+done
