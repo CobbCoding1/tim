da6d687cdd7ba7c462784e6c75041847de4c0ecf
CobbCoding <rircobb@gmail.com>
2023-10-19 18:53:22 -0400

    cleaned up a few things

--- tasm.c
+++ tasm.c
@@ -15,29 +15,10 @@ Inst pop_program(Inst program[], int program_size){
 Inst *generate_instructions(ParseList *head, int *program_size){
     Inst *program = malloc(sizeof(Inst));
     Inst_Set insts[INST_COUNT + 1] = {    
-            INST_NOP,
-            INST_PUSH,
-            INST_POP,
-            INST_DUP,
-            INST_INDUP,
-            INST_SWAP,
-            INST_INSWAP,
-            INST_ADD,
-            INST_SUB,
-            INST_MUL,
-            INST_DIV,
-            INST_MOD,
-            INST_ADD_F,
-            INST_SUB_F,
-            INST_MUL_F,
-            INST_DIV_F,
-            INST_MOD_F,
-            INST_CMPE,
-            INST_CMPNE,
-            INST_CMPG,
-            INST_CMPL,
-            INST_CMPGE, INST_CMPLE, INST_JMP, INST_ZJMP,
-            INST_NZJMP, INST_PRINT, INST_HALT, INST_COUNT};
+        INST_NOP, INST_PUSH, INST_POP, INST_DUP, INST_INDUP, INST_SWAP, INST_INSWAP, INST_ADD, INST_SUB, INST_MUL, 
+        INST_DIV, INST_MOD, INST_ADD_F, INST_SUB_F, INST_MUL_F, INST_DIV_F, INST_MOD_F, INST_CMPE, INST_CMPNE, INST_CMPG, 
+        INST_CMPL, INST_CMPGE, INST_CMPLE, INST_JMP, INST_ZJMP, INST_NZJMP, INST_PRINT, INST_HALT, INST_COUNT};
+
     while(head != NULL){
         assert(head->value.type != TYPE_NONE && "Value should not be none\n");
         assert(head->value.type < (TokenType)INST_COUNT);
